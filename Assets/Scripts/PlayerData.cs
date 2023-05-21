0acb49db10012cdd9e594d958c6f3fcc04d020f4
Jacob Lacsamana <JLacsamana@bgcengineering.ca>
2023-05-21 05:52:38 -0700

    setup more ui elements

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    Quest currentActiveQuest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentActiveQuest = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Quest GetCurrentQuest()
+    {
+        return currentActiveQuest;
+    }
+
+    public void RemoveQuest()
+    {
+        currentActiveQuest = null;
+    }
+}
---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest
+{
+    public string questTitle;
+    public string questDescription;
+
+
+}
