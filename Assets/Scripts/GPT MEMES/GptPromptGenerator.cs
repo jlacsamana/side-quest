b06221fbef6f76a96d015adde97c5989ee951136
Jacob Lacsamana <JLacsamana@bgcengineering.ca>
2023-05-21 04:41:56 -0700

    clean push

---
+++ Assets/Scripts/GPT MEMES/GptPromptGenerator.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GptPromptGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Setup necessary things to make API calls to CHATGPT
+
+
+        // ask GPT something by making an API call
+
+
+
+
+
+        // JUST FOR TESTING: print out what it gives you with Debug.log()
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/NavController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class NavController : MonoBehaviour
+{
+    [SerializeField]
+    UIDocument startMenu;
+
+    Button startBtn;
+
+    [SerializeField]
+    UIDocument questMenu;
+    [SerializeField]
+    UIDocument userInfoPanel;
+    [SerializeField]
+    UIDocument settingsPanel;
+
+    //alerts
+    [SerializeField]
+    UIDocument IngameUI;
+    [SerializeField]
+    UIDocument questAccept;
+    [SerializeField]
+    UIDocument noQuestTaken;
+    [SerializeField]
+    UIDocument questCompleted;
+
+
+
+    private void Awake()
+    {
+        startBtn = startMenu.rootVisualElement.Q("StartButton") as Button;
+        startBtn.clicked += StartBtnClicked;
+    }
+
+    private void StartBtnClicked()
+    {
+        SceneManager.LoadScene("MapViewScene");
+        startMenu.enabled = false;
+        IngameUI.enabled = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
